7d1364cc73adead55fbcaaec2bb96bcdc2d3a29e
Harshada Ramesh Gujar <harshadarameshgujar@Harshadas-MacBook-Air.local>
2026-01-11 11:04:09 +0530

    Update app1.py to use environment variables

--- app1.py
+++ app1.py
@@ -181,10 +181,11 @@ class PDF(FPDF):
 
 # Email function using Hostinger SMTP
 def send_email_with_pdf(to_email, pdf_path):
-    from_email = "info@h2rsbitwaveacademy.in"
-    password = "Info@h2rsbitwave"  # Replace with actual mailbox password
-    smtp_server = "smtp.hostinger.com"
-    smtp_port = 465  # SSL
+    from_email = os.getenv("MAIL_USERNAME")
+    password = os.getenv("MAIL_PASSWORD")
+    smtp_server = os.getenv("SMTP_SERVER", "smtp.hostinger.com")
+    smtp_port = int(os.getenv("SMTP_PORT", 465))
+
 
     msg = MIMEMultipart()
     msg["From"] = from_email
